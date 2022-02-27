defecf57bd214a6e98fff96c599bfb56c1044dfd
xingmeichen <xingmeichen12@126.com>
2022-02-27 20:12:45 +0800

    remove unused import

--- src/test/java/com/mabel/UserTest.java
+++ src/test/java/com/mabel/UserTest.java
@@ -5,7 +5,6 @@ import com.mabel.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @project: message-gateway
